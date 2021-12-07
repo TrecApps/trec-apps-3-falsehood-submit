e98d901ceb107b5361c62947fc29f91e13779e30
John Jacko <jljacko@outlook.com>
2021-12-07 15:04:45 -0600

    Updated To use Cosmos DB upon launch

--- src/main/java/com/trecapps/falsehoods/submit/Driver.java
+++ src/main/java/com/trecapps/falsehoods/submit/Driver.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.trecapps.falsehoods.submit","com.trecapps.falsehoods.falsehoodSearch"})
+@ComponentScan({"com.trecapps.base.FalsehoodModel","com.trecapps.base.InfoResource"})
 public class Driver {
 
     public static void main(String[] args) {
