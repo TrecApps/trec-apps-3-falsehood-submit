de517d28615ac49d723f560271f72799e2a2a76b
John Jacko <jljacko@outlook.com>
2021-12-29 20:38:43 -0700

    Updated Azure AD config

--- src/main/java/com/trecapps/falsehoods/submit/security/SecurityConfig.java
+++ src/main/java/com/trecapps/falsehoods/submit/security/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.trecapps.falsehoods.submit.security;
 
 import com.azure.spring.aad.webapp.AADOAuth2UserService;
+import com.azure.spring.autoconfigure.aad.AADAuthenticationProperties;
 import com.trecapps.base.FalsehoodModel.repos.FalsehoodUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
-@Component
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    AADOAuth2UserService getAadoAuth2UserService(AADAuthenticationProperties aadAuthProps)
+    {
+        return new AADOAuth2UserService(aadAuthProps);
+    }
 
     @Autowired
     AADOAuth2UserService aadoAuth2UserService;
